883cb31ad7547555743afd9bbd52240d643ef552
LordIcecube <148607118+LordIcecube@users.noreply.github.com>
2023-10-25 15:36:20 -0500

    Update d20.py

--- d20.py
+++ d20.py
@@ -1,6 +1,6 @@
 import random #Transfers the random function.
 
-while True: #If Import is transfered
+while True: #While the Import random is true, the system will do these functions.
     input ("Press the enter key to roll.") #Hit enter to roll
     d20 = random.randint (1,20) #Chooses random integer between 1 and 20, creates a d20 variable
     print (d20) #printing the function
